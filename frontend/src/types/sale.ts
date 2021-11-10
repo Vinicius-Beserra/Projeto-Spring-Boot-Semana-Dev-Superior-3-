90d2a9023072e998597e2d92dd73df1cde610bad
vinicius <viniciusbeserra1341@gmail.com>
2021-11-10 16:07:23 -0300

    Datatable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { seller } from "./seller";
+
+export type sale = {
+  id: number;
+  visited: String;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: seller;
+};
+
+export type SalePage = {
+  content?: sale[];
+  last: boolean;
+  totalElements: number;
+  totalPages: number;
+  size?: number;
+  number: number;
+  first: boolean;
+  numberOfElements?: number;
+  empty?: number;
+};
+
 export type SalesSum = {
   sellerName: string;
   sum: number;
